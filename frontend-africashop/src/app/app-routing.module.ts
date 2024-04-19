b29b8f564b1191e4ce378f61846df10bd7ba3aa9
Denis <100221021+bidjdenis@users.noreply.github.com>
2024-04-19 04:24:33 +0200

    login + register component

--- frontend-africashop/src/app/app-routing.module.ts
+++ frontend-africashop/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:"", component: HomeComponent},
+
+{
+  path:'auth-pages',
+  loadChildren: () => import("./auth-pages/auth-pages.module").then((p) => p.AuthPagesModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
