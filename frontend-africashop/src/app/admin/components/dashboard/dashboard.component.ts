0dcfbf0633ede4b693d3d819100c23af3d57e9c3
Denis <100221021+bidjdenis@users.noreply.github.com>
2024-04-28 00:11:21 +0200

    admin dashboard + product search

--- frontend-africashop/src/app/admin/components/dashboard/dashboard.component.ts
+++ frontend-africashop/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { Product } from '../../../payload/product';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+
+  public products : Product[] = [];
+  public filteredProducts: Product[] = [];
+  public searchKeyword: string = '';
+
+  constructor(private adminService : AdminService){}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+
+  }
+
+  getAllProducts(){
+    this.products = [];
+    this.adminService.getAllProduct().subscribe(res => {
+      res.forEach((product : Product) =>  {
+        product.processedImg ="data:image/jpeg; base64," + product.byteImg
+        this.products.push(product)
+        this.applySearchFilter(); 
+      }
+    )
+    })
+  }
+
+  applySearchFilter(): void {
+    if (this.searchKeyword.trim() !== '') {
+      this.filteredProducts = this.products.filter((product: Product) =>
+        product.name.toLowerCase().includes(this.searchKeyword.toLowerCase()),
+      );
+    } else {
+      this.filteredProducts = this.products;
+    }
+  }
+
+  onSearchChange(): void {
+    console.log(this.applySearchFilter()); 
+  }
 
 }
